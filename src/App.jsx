4f330f25b515ebff9d92595d95200d02fafef6e4
Shakil Ahmed <25431335+beshakil@users.noreply.github.com>
2024-02-01 17:28:57 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import KanbanBoard from './components/KanbanBoard'
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold underline">
-        Hello world!
-      </h1>
       <KanbanBoard/>
     </>
   )
