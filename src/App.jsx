b979785f2b6f4e709226fee01f3bc6705d51da1f
Shakil Ahmed <dev.shakilshajib@gmail.com>
2024-02-01 13:33:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import KanbanBoard from './components/KanbanBoard'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+      <KanbanBoard/>
+    </>
+  )
+}
+
+export default App
