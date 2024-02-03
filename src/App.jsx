9346f728d817f3743dfb8daf710d2ab85a620995
beshakil <shakil15-7018@diu.edu.bd>
2024-02-04 01:44:01 +0600

    erth

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import KanbanBoard from './components/KanbanBoard'
 function App() {
   return (
     <>
-    <p className='text-center'></p>
+    <h1 className='text-center text-2xl'>Kanvan View</h1>
       <KanbanBoard/>
     </>
   )
